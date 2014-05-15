76565787fbfb6e34a0a3065bba3127b3752f718c
ashitha <ashi.m.shetty@gmail.com>
2014-05-15 14:15:23 +0530

    Update js_util.js

--- js_util.js
+++ js_util.js
@@ -1,15 +1,1 @@
-console.log = function(){
-    var args = Array.prototype.slice.call(arguments, 0);
-    var op= document.createElement('output');
-    var logdiv=document.getElementById('output');
-    if(logdiv==null)
-    {
-            var newdiv=document.createElement('div');
-        newdiv.id="output"
-        document.body.appendChild(newdiv);
-    }
-    logdiv=document.getElementById('output');
-          document.getElementById('output').innerHTML += args.join(" ") + "\n";
-    
-    
-};
+alert('efe');
